4c2fd7c5b79af65c9e8ab63a1239b69ae5325ec2
John Waite <mattekure@gmail.com>
2025-03-08 21:20:15 -0500

    Start of Image.lua

---
+++ library/FG-API/Packages/Image.lua
@@ -0,0 +1,147 @@
+---@meta
+---@module tokeninstance
+---@module databasenode
+
+---@class Image
+Image = {}
+
+---Attempts to add a layer on the specified image value database node. Only scripts running on the session host can run this API.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---Valid layer types are: image, paint (default), text
+---• Image layer type fields: name (string), asset (string), x (number), y (number), w (number), h (number), color (string), angle (number), invisible (boolean), gmonly (boolean), locked (boolean)
+---• Note: w and h attributes are specified in grid units.
+---• Paint layer type fields: name (string)
+---• Text layer type fields: text (string), font (string), fontsize (number)
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@param layertype string The desired layer type to add\
+---@param layerdata table A table containing string-based keys to denote layer type specific parameters (see above).
+---@return number|nil # Returns the layer ID of the created layer, or nil if no layer created.
+function Image.addLayer(dbnodeORdbpath, layertype, layerdata) end
+
+---Attempts to add a layer on the specified image value database node. Only scripts running on the session host can run this API.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---• Paint stamp data fields: asset (string), x (number), y (number), w (number), h (number), color (string), angle (number)
+---Note: w and h attributes are specified in grid units.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@param layerid number The desired layer to edit.function Image.addLayerPaintStamp() end
+---@param stampdata table A table containing string-based keys to denote paint stamp specific parameters (see above).
+---@return number|nil # Returns the layer ID of the created layer, or nil if no layer created.
+function Image.addLayerPaintStamp(dbnodeORdbpath, layerid, stampdata) end
+
+---Attempts to delete the specified layer ID on the specified image value database node. Only scripts running on the session host can run this API.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@param layerid number The desired layer to delete
+function Image.deleteLayer(dbnodeORdbpath, layerid) end
+
+---Retrieves the distance base units of the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return number # Returns the distance base units.
+function Image.getDistanceBaseUnits(dbnodeORdbpath) end
+
+---Returns the distance (in game system units) between two tokens (and/or points) of the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated. If a token parameter specifies a token not on the image value, then zero will be returned.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@param tokena tokeninstance|number|table A tokeninstance object, OR the ID of a tokeninstance, OR a Lua table of the form { 1 = #, 2 = # } or { x = #, y = # }, indicating a point on the image.
+---@param tokenb tokeninstance|number|table A tokeninstance object, OR the ID of a tokeninstance, OR a Lua table of the form { 1 = #, 2 = # } or { x = #, y = # }, indicating a point on the image.
+---@return number Returns the distance between the two specified tokens and/or points in game system units.
+function Image.getDistanceBetween(dbnodeORdbpath, tokena, tokenb) end
+
+---Retrieves the distance diagonal multiplier of the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return number # Returns the distance diag multiplier of the specified image value. [>0 for grid-based multiple; 0 for raw calculation]
+function Image.getDistanceDiagMult(dbnodeORdbpath) end
+
+---Retrieves the distance display suffix of the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string
+---@return string # Returns the distance display suffix of the specified image value.
+function Image.getDistanceSuffix(dbnodeORdbpath) end
+
+---Returns the offset of the first grid square in the top left corner on the specified image value database node. The offsets will be different from zero as a result of the grid not being perfectly aligned with the top left corner of the image itself. The values will vary from zero to -((grid size) - 1).
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return number # Returns the horizontal offset of the grid
+---@return number # Returns the vertical offset of the grid
+function Image.getGridOffset(dbnodeORdbpath) end
+
+---Returns the size of a grid square on the specified image value database node. The grid is rectangular, i.e. the height and width of the squares is the same.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return number # Returns the size of a grid square.
+function Image.getGridSize(dbnodeORdbpath) end
+
+---Returns whether grid snapping is enabled on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return number|nil # Returns 1 if enabled, 0 if disabled, or nil if undefined.
+function Image.getGridSnap(dbnodeORdbpath) end
+
+---Returns the type of grid currently active on the specified image value database node. Valid types are "square", "hexcolumn" (hexes stacked vertically) and "hexrow" (hexes stacked horizontally).
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return string Returns the grid type.
+function Image.getGridType(dbnodeORdbpath) end
+
+---Attempts to get the layer ID matching the specified name on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried
+---@param name string The desired layer name to query
+---@return number|nil # Returns the layer ID of the desired layer, or nil if no matching layer found.
+function Image.getLayerByName(dbnodeORdbpath, name) end
+
+---Returns the overall size of the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return number # Returns the image width.
+---@return number # Returns the image height.
+function Image.getSize(dbnodeORdbpath) end
+
+---Returns whether token movement has been locked for clients on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return boolean # Returns the current lock state.
+function Image.getTokenLockState(dbnodeORdbpath) end
+
+---Retrieve a list of all tokens on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return table # A table with integer index values containing references to tokeninstance objects.
+function Image.getTokens(dbnodeORdbpath) end
+
+---Returns a list of all tokens within the given game system unit distance from the specified token or point on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated. If a token parameter specifies a token not on the image value, then an empty list will be returned.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@param token tokeninstance|number|table A tokeninstance object, OR the ID of a tokeninstance, OR a Lua table of the form { 1 = #, 2 = # } or { x = #, y = # }, indicating a point on the image.
+---@param distance number The range in game system units that should be considered for distance calculations.
+---@return table # A table with integer index values containing references to tokeninstance objects.
+function Image.getTokensWithinDistance(dbnodeORdbpath, token, distance) end
+
+---Returns whether the grid is visible on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return boolean # Returns true if the grid is visible, or false if not.
+function Image.hasGrid(dbnodeORdbpath) end
+
+---Returns whether there are any tokens placed on the specified image value database node.
+---If the specified database node does not exist or is not an image value database node, then an error is generated.
+---@param dbnodeORdbpath databasenode|string Specifies the database node whose state is being queried.
+---@return boolean # Returns true if there is at least one token placed, or false if not.
+function Image.hasTokens(dbnodeORdbpath) end
+
+---Returns whether the specified image value database node is an image value database node.
+---@param dbnodeORdbpath databasenode|string  Specifies the database node whose state is being queried.
+---@return boolean # Returns true if the database node exists and is an image value database node, or false if not.
+function Image.isImage(dbnodeORdbpath) end
+
+function Image.setDistanceBaseUnits() end
+function Image.setDistanceDiagMult() end
+function Image.setDistanceSuffix() end
+function Image.setGridOffset() end
+function Image.setGridSize() end
+function Image.setGridSnap() end
+function Image.setGridType() end
+function Image.setTokenLockState() end
+function Image.snapToGrid() end
